dd2ed18039a4367f7a8c742629f956b60ebaef0d
Vyom88 <vyom.kapadia@gmail.com>
2021-07-29 12:17:48 -0400

    adding files

---
+++ register/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.db import transaction
+
+from .models import Student, User
+
+class StudentSignUpForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+    @transaction.atomic
+    def save(self):
+        user = super().save(commit=False)
+        user.is_student = True
+        user.save()
+        student = Student.objects.create(user=user)
+        return user
