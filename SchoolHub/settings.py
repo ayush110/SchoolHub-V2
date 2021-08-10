c6057a225501ca654fc07fa34dd92df4f6eb972d
Vyom88 <vyom.kapadia@gmail.com>
2021-08-09 20:43:20 -0400

    deleted useless imports

--- SchoolHub/settings.py
+++ SchoolHub/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "register.apps.RegisterConfig",
     'crispy_forms',
-    'widget_tweaks',
+    # 'widget_tweaks',
     'main.apps.MainConfig',
 ]
 
